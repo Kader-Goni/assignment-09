4483605f3e5e435ce4c43361b8f7c039431f428e
Kader-Goni <kadergoni35@gmail.com>
2022-04-05 22:12:35 +0600

    Reviews Updated

--- src/component/Review/Review.js
+++ src/component/Review/Review.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import useReviews from '../../Hooks/useReviews';
-import StudentReview from '../Home/StudentReview';
-import "./review.css"
+import React from "react";
+import useReviews from "../../Hooks/useReviews";
+import StudentReview from "../Home/StudentReview";
+import "./review.css";
 
 const Review = () => {
-    const [reviews, setReview] = useReviews();
-    return (
-        <div>
-            <h1 className='text-center fw-bold my-4'><span className='text-primary'>Students</span> Reviews</h1>
-            <div className="container Review-container my-4">
-                {
-                    reviews.map(review => <StudentReview
-                        key={review.id}
-                        review={review}
-                    ></StudentReview>)
-                }
-            </div>
-        </div>
-    );
+  const [reviews, setReview] = useReviews();
+  return (
+    <div>
+      <h1 className="text-center fw-bold my-4">
+        <span className="text-primary">Students</span> Reviews
+      </h1>
+      <div className="container Review-container my-4">
+        {reviews.map((review) => (
+          <StudentReview key={review.id} review={review}></StudentReview>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Review;
