944a29ae77f99b92dd50c85702119a25be79da66
Kader-Goni <kadergoni35@gmail.com>
2022-04-05 21:26:09 +0600

    DashBoard Added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './component/Home/Home';
 import NavBar from './component/Navbar/NavBar';
 import Review from './component/Review/Review';
-// import Dashboard from "./component/Deshbord/Dashboard"
+import Dashboard from "./component/Deshbord/Dashboard"
 import Blog from "./component/Blog/Blog"
 import About from "./component/About/About"
 import ErrHadel from "./component/ErrHadel/ErrHandel"
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/review" element={<Review></Review>}></Route>
-        {/* <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route> */}
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route path="/blog" element={<Blog></Blog>}></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="*" element={<ErrHadel></ErrHadel>}></Route>
